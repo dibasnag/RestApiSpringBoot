558b270c5493b986acd806cd308a2bab868f5de2
dibasnag <dnag-consultant@scholastic.com>
2020-02-12 14:36:56 +0530

    Code Cleanup

--- src/main/java/com/example/demo/StudentService.java
+++ src/main/java/com/example/demo/StudentService.java
@@ -14,54 +14,25 @@ public class StudentService {
 	@Autowired
 	private StudentRepository studentRepositiory;
 	
-		//	private List<Student> students = new ArrayList<>(Arrays.asList(
-		//			new Student("1", "Student One", "School1", "STD I"),
-		//			new Student("2", "Student Two", "School2", "STD II"),
-		//			new Student("3", "Student Three", "School3", "STD III")
-		//			));
 	
 	public List<Student> getAllStudents() {
-		//return students;
-		
-		//List<Student> students = new ArrayList<>();
-		//studentRepositiory.findAll().forEach(students::add);
-		//return students;
-		
 		return (List<Student>) this.studentRepositiory.findAll();
 	}
 
 	public Student getStudent(String id) {
-		//return students.stream().filter(s -> s.getId().equals(id)).findFirst().get();
-		//return studentRepositiory.findById(id);
 		Student student = studentRepositiory.findById(id).orElse(null);
 		return student;
 	}
 	
 	public void addStudent(Student student) {
-		//students.add(student);
 		studentRepositiory.save(student);
 	}
 
 	public void updateStudent(String id, Student student) {
-		//		for(int i=0; i<students.size(); i++) {
-		//			Student s = students.get(i);
-		//			if(s.getId().equals(id)) {
-		//				students.set(i, student);
-		//				return;
-		//			}
-		//		}
-		
-		//If only some fields to be updated are provided along with ID.
-		//Then first find the object then modify it and then save it.
-		//Student student = studentRepositiory.findById(id).orElse(null); 
-		//student.setName("NEW NAME"); 
-		//studentRepositiory.save(student);
-		
 		studentRepositiory.save(student);
 	}
 
 	public void deleteStudent(String id) {
-		//students.removeIf(s -> s.getId().equals(id));
 		studentRepositiory.deleteById(id);
 	}
 	
